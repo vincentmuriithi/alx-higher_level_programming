36ca49496c3888b1e3c2878a00306e345c74f8e8
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-10-04 13:29:01 -0700

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def fizzbuzz():
-    for k in range(101):
+    for k in range(1, 101):
         if k % 5 == 0 and k % 3 == 0 and k > 0:
             print("FizzBuzz", end=" ")
         elif k % 3 == 0 and k > 0:
