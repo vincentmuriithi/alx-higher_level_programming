0c2e3693eb46f8bd23f2afe1383a41ccfe6d040a
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-10-04 11:54:27 -0700

    6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for first_digit in range(10):
+    for second_digit in range(first_digit + 1, 10):
+        if first_digit != second_digit:
+            if first_digit == 8 and second_digit == 9:
+                print("{:d}{:d}".format(first_digit, second_digit))
+            else:
+                print("{:d}{:d}, ".format(first_digit, second_digit), end="")
