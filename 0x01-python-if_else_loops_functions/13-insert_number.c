0d54c1c755169cb22dba5c307137edf75cdcca36
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-10-05 07:36:38 -0700

    13-insert_number.c

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+* insert_node - inserts a namba in a sorted singly list
+* @head: input pointer
+* @number: umber to be inserted
+* Return: returns the list
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+listint_t *new = *head;
+listint_t *node =  malloc(sizeof(listint_t));
+
+if (node)
+return (NULL);
+node->n = number;
+node->next = NULL;
+
+if (new == NULL || (new->next == NULL && new->n >= number))
+{
+node->next = new;
+*head = node;
+return(node);
+}
+while (new && new->next != NULL && new->n <= number)
+{
+new = new->next;
+
+}
+new->next = node;
+return(node);
+
+}
