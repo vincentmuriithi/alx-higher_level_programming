e8f29859533c831d0b282b62fd7edf787a918bb6
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-10-11 09:17:48 -0700

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new = ""
+    for k in my_string:
+        if my_string[k] != 'c' and my_string[k] != 'C':
+            result += k
+
+        return char
