ae45cc70236be92f71569ed70654ccaaa03fd58d
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-10-23 19:29:37 -0700

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+def safe_print_list(my_list=[], x=0):
+    try:
+
+        if x != 0:
+            for i in range(x + 1):
+                print(my_list[i])
+    except:
+        print("error occurred")
