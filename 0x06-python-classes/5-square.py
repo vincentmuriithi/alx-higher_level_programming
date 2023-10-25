9759278f81f68b49b59e24457dd300f1955deae4
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-10-24 19:18:06 -0700

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+class Square:
+
+    def __init__(self, size = 0):
+        self.size = size
+
+    def area(self):
+        return self.__size**2
+
+    @property
+    def size(self):
+        self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must > 0")
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for k in range(self.__size):
+                    print("#", end = "")
+                print()
