921a7f9fd6e215b993c2dc7a11356db77135a3de
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-11-03 06:07:09 -0700

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,2 +1,11 @@
+#!/usr/bin/python3
+"""0-rectangle, built for Alx  project 0x08 task 0.
+"""
+
+
 class Rectangle:
-    pass
+    """Empty class per task instructions, will be built upon in later tasks.
+    """
+
+
+pass
