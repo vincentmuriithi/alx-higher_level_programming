e8bbf4747d93ad5f99ec260ae7e3444e1afd4321
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-11-13 10:26:13 -0800

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,14 +8,18 @@ class Base:
      Attributes:
         id (int): Identity of each instance.
     """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Creates new instances of Base.
+
+        """
+        Creates new instances of Base.
 
         Args:
             id (int, optional): Identity of each instance. Defaults to None.
         """
+
         if id is not None:
             self.id = id
         else:
