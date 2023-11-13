d2f026aae69ef3ab28d0495bfa707fd511c2afc0
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-11-13 10:11:01 -0800

    base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+
+        if id is not None:
+            self.id = id
+        else:
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
