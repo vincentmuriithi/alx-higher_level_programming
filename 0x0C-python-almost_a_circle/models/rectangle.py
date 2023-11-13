488a277da04b2fc4b271a0862617ca0acf2daf22
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-11-13 10:34:45 -0800

    rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+Defines a class Rectangle
+"""
+
+class Rectangle(Base):
+    """
+    class Rectangle inherits from class Base
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Creates new instances of rectangle.
+
+        Args:
+            width (int): width of rectangle.
+            height (int): height of rectangle.
+            x (int, optional): x. Defaults to 0.
+            y (int, optional): y. Defaults to 0.
+            id (int, optional): Identity number of rectangle. Defaults to None.
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__()
