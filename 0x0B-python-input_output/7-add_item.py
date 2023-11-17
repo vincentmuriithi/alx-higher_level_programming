61185a01b481726d9e804bd410193f503fa900f5
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-11-17 01:18:06 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,24 +1,51 @@
 #!/usr/bin/python3
+
 """
-Adds all arguments to a Python list and saves them to a file.
+module to add items into python list
 """
+
 import sys
 import json
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
 
-# Create the file if it doesn't exist
-open("add_item.json", "a").close()
 
+def save_to_json_file(my_obj, filename):
+    """
+    Save the given object to a text file using JSON representation.
+
+    Parameters:
+        my_obj: Any valid JSON-serializable object.
+        filename: The name of the file to which the object will be saved.
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(my_obj, file)
+
+
+def load_from_json_file(filename):
+    """
+    Create a Python object from a JSON file.
+
+    Parameters:
+        filename (str): The name of the JSON file to read.
+
+    Returns:
+        object: The Python object created from the JSON file.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
+
+
+# Load existing data from the file or create an empty list
 try:
-    # Load existing data from the file
-    my_list = load_from_json_file("add_item.json")
-except json.JSONDecodeError:
-    # If the file is empty or not valid JSON, initialize with an empty list
-    my_list = []
+    existing_data = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    existing_data = []
 
 # Add command-line arguments to the list
-my_list.extend(sys.argv[1:])
+for arg in sys.argv[1:]:
+    existing_data.append(arg)
 
 # Save the updated list to the file
-save_to_json_file(my_list, "add_item.json")
+save_to_json_file(existing_data, 'add_item.json')
+
+print("Arguments added to 'add_item.json'")
