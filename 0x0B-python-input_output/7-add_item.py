a395e0c5c629f83429cb8a90eeda9e0191941b22
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-11-17 00:52:12 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,15 +4,19 @@
 adds all arguments to a Python list
 """
 
+
 import sys
+import json
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
 
-
-save = __import__("5-save_to_json_file").save_to_json_file
-load = __import__("6-load_from_json_file").load_from_json_file
-
+# Create the file if it doesn't exist
 open("add_item.json", "a").close()
+
 try:
-    i = load("add_item.json")
-except ValueError:
+    i = load_from_json_file("add_item.json")
+except json.JSONDecodeError:
     i = []
-save(i + sys.argv[1:], "add_item.json")
+
+# Append command-line arguments to the list
+save_to_json_file(i + sys.argv[1:], "add_item.json")
