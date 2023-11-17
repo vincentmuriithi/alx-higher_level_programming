461551487767fdb08b91cec308cc0467ec8e42fa
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-11-16 23:30:41 -0800

    6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -20,5 +20,5 @@ def load_from_json_file(filename):
         object: The Python object created from the JSON file.
     """
 
-    with open(filename, "r",  encoding="utf-8") as f:
-        json.load(f)
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)
