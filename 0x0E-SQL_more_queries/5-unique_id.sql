6adbd16a19c5a0be7615f6c5bc3aa3b73217573e
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-12-16 18:28:58 -0800

    9-cities_by_state_join.sql

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- Script creates the table unique_id on your MySQL server.
+CREATE TABLE IF EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- Script hat creates the database hbtn_0d_usa and 
+-- the table cities (in the database hbtn_0d_usa) on your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	PRIMARY KEY(id),
+	id INT NOT NULL UNIQUE AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
+	state_id INT NOT NULL,
+	FOREIGN KEY(tate_id) REFERENCES states(id)
+	);
