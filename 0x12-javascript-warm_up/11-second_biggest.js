8d9370ceeee330e80d4d0ba419913f017bcf1e9d
vincentmuriithi <vincentmuriithi06@gmail.com>
2024-01-24 06:00:15 -0800

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-
-// Searches the second biggest integer in the list of arguments
-
-if (process.argv.length < 4) {
-  console.log('0');
+if (process.argv.length <= 3) {
+  console.log(0);
 } else {
-  const args = process.argv.slice(2);
-  args.sort((a, b) => b - a);
-  console.log(args[1]);
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
