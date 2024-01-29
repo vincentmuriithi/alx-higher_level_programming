c22c15b8eb41823438bbb8c58877f7c78a7997c4
vincentmuriithi <vincentmuriithi06@gmail.com>
2024-01-29 09:24:43 -0800

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,16 @@
-#!/usr/bin/env python3
-#This script fethes a url usind urllib
+#!/usr/bin/python3
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
 
-import urllib.request as r
 
-req = r.Request("https://alx-intranet.hbtn.io/status")
-with r.urlopen(req) as response:
-    response.read()
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
