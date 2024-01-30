81c19524ae0624d2a1085fb1eddf865acca609f4
vincentmuriithi <vincentmuriithi06@gmail.com>
2024-01-30 07:33:46 -0800

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-Pyhthon script that sends a request to the URL and displays:
+Python script that sends a request to the URL and displays:
     - The body of the response if there are no errors
     - The error code when there is an HTTP error.
 """
