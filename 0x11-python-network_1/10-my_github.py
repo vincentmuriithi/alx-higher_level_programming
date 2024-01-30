95c947a71a25ccf4d8b4ba0eb5222d54983c8d8b
vincentmuriithi <vincentmuriithi06@gmail.com>
2024-01-30 15:34:27 -0800

    10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-A script that takes GitHub credentilas and uses the GitHub API to display the user id
+Python script that takes GitHub credentials
+and uses the GitHub API to display the user id
 """
-
 from requests import get, auth
 import sys
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     url = 'https://api.github.com/user'
     user = sys.argv[1]
     password = sys.argv[2]
