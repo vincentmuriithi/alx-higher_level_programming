56a4f74476e156519a9a04f1018f4496815b29bb
vincentmuriithi <vincentmuriithi06@gmail.com>
2024-02-24 11:25:21 -0800

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-//Script that writes a string to a file
-
-const fs = require("fs");
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-fs.writeFile(filePath, content, "utf-8", (err)=>{
-	if (err)
-		console.log(err);
+const fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
