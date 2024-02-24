9fa742a8a87487fff860d74f3118dc0d6a3272cc
vincentmuriithi <vincentmuriithi06@gmail.com>
2024-02-24 11:33:26 -0800

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
-// Script  that reads and prints the content of a file
-const fs = require("fs");
-const filePath = process.argv[2];
-fs.readFile(filePath, "utf-8", (err, data)=>{
-if (err)
-console.log(err);
-else
-console.log(data);
-}
-);
+// Script that reads and prints the content of a file
+
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
