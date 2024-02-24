15ecc6bec105f272bea3826d16e50dec7f306ad9
vincentmuriithi <vincentmuriithi06@gmail.com>
2024-02-24 11:27:48 -0800

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 // Script  that reads and prints the content of a file
-
 const fs = require("fs");
 const filePath = process.argv[2];
-
 fs.readFile(filePath, "utf-8", (err, data)=>{
 	if (err)
 		console.log(err);
