12aec0d041020487a2b11bcd8ac75cca58849e9b
vincentmuriithi <vincentmuriithi06@gmail.com>
2024-02-24 20:21:56 -0800

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-//Script that prints the title of a star war movie
+// Script that prints the title of a Star Wars movie where the episode number matches a given integer
 
 const request = require('request');
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const urlApi = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(url, function(error, response, body){
-if (error)
-console.error(error);
-else
-console.log(JSON.parse(body).title);
+request(urlApi, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
