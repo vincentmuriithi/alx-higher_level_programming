cfe505c365f93ef5db9c1cb0c63854cc28c18a32
vincentmuriithi <vincentmuriithi06@gmail.com>
2024-02-25 04:09:57 -0800

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,32 +1,23 @@
 #!/usr/bin/node
-//Script that computes the number of tasks completed by user id.
+// Script that computes the number of tasks completed by user id
 
-const request = require("request");
-const url = process.argv[2];
+const request = require('request');
 
-request(url, (error, response, body)=> {
-	if (error){
-		console.error('Error: ', error);
-		return;
-	}
-
-	if (response.statusCode !== 200){
-		console.error('Invalid response', response.statusCode);
-		return;
-	}
-
-	const todos = JSON.parse(body);
-	const completedTasksByUser = {};
-
-	todos.forEach(todo=> {
-		if (todo.completed) {
-			if (completedTasksByUser[todo.userId]) {
-				completedTasksByUser[todo.userId]++;
-			}
-			else{
-				completedTasksByUser[todo.userId] = 1;
-			}
-		}
-	});
-	console.log(completedTasksByUser);
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const dict = JSON.parse(body).reduce((acc, elem) => {
+    if (!acc[elem.userId]) {
+      if (elem.completed) {
+        acc[elem.userId] = 1;
+      }
+    } else {
+      if (elem.completed) {
+        acc[elem.userId] += 1;
+      }
+    }
+    return acc;
+  }, {});
+  console.log(dict);
 });
