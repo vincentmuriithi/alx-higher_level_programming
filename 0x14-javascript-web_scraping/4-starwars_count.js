a3c6a2aa091a4e68b9ca3ec9ece109b54fad5995
vincentmuriithi <vincentmuriithi06@gmail.com>
2024-02-24 20:48:55 -0800

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,13 @@
 #!/usr/bin/node
-//Script that that prints the number of movies where the character “Wedge Antilles” is present.
+// Script that prints the number of movies where the character “Wedge Antilles” is present
 
-const request = require("request");
-const url = process.argv[2];
-const characterId = 18;
-
-request(url, (error, response, body)=> {
-	if (error)
-		console.error(error);
-	else{
-		const filmData = JSON.parse(body).results;
-		let k = 0;
-		filmData.forEach(film => {
-			if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`))
-				k++;
-		});
-	console.log(k);
-	}
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const nb = JSON.parse(body).results.filter((elem) => {
+    return elem.characters.filter((url) => { return url.includes('18'); }).length;
+  }).length;
+  console.log(nb);
 });
